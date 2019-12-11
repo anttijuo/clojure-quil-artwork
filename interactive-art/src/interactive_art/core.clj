0576c26b4ba52080d2a7054ec57806b2d2d8306d
antti juotasniemi <antti.juotasniemi@siili.com>
2019-12-11 19:39:12 +0200

    Update logic to simply draw one smiley in prep for manipulation

--- interactive-art/src/interactive_art/core.clj
+++ interactive-art/src/interactive_art/core.clj
@@ -1,16 +1,8 @@
-(ns mushrooms.core
+(ns skulls.core
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
 (def field-size 700)
-
-(defn pulse [low high rate]
-  (let [diff (- high low)
-        half (/ diff 2)
-        mid (+ low half)
-        s (/ (q/millis) 1000.0)
-        x (q/sin (* s (/ 1.0 rate)))]
-    (+ mid (* x half))))
 
 (defn load-image [image]
   (let [path (str "images/" (name image) ".png")]
@@ -30,10 +22,10 @@
   state)
 
 (defn skull [x y state]
-  (q/rotate (pulse -0.02 0.02 0.5))
-  (q/translate (pulse 1 50 0.25) 0)
+  ;(q/rotate (pulse -0.02 0.02 0.5))
+  ;(q/translate (pulse 1 50 0.25) 0)
   (q/image (get state :head) x y)
-  (q/translate 0 (pulse 1 100 0.25))
+  ;(q/translate 0 (pulse 1 100 0.25))
   (q/image (get state :jaw) x y))
 
 (defn wild-skull [x y state scale]
@@ -43,19 +35,10 @@
 
 (defn draw-state [state]
   (q/clear)
-  (skull (/ (q/width) 2) (/ (q/height) 2) state)
-  ; replace with repeatedly
-  (loop [n 5 x 100 y 100 scale 0.5]
-    (if (zero? n) nil
-        (do 
-          (wild-skull x y state scale)
-          (recur (dec n) (rand-int 1000) (rand-int 1000) (rand))
-          )))
-  ;
-          )
+  (skull (/ (q/width) 2) (/ (q/height) 2) state))
 
-(q/defsketch mushrooms
-  :host "mushrooms"
+(q/defsketch skulls
+  :host "skulls"
   :size [1000 1000]
   :setup setup
   :update update-state
